f0fc8991f6b3c69b71403635706480cf4f9d4aa1
liupold <rohn.ch@gmail.com>
2019-12-01 15:57:24 +0530

    setup.sh (a simple setup script for config files) is added.

---
+++ setup.sh
@@ -0,0 +1,30 @@
+echo "Starting..."
+
+# backup and copy
+files=`ls -a | sed -e 's/README.md//' | sed -e 's/LICENSE//' | sed -e 's/.git//' | sed -e 's/setup.sh//'`
+for entry in $files
+do
+	if [ -f "$entry" ];then
+		if [ -f "$HOME/$entry" ]
+		then
+			mv "$HOME/$entry" "$HOME/$entry.bak"
+		fi
+		ln -s $(realpath "$entry") "$HOME/$entry"
+	elif [[ -d "$entry" && "$entry" != "." && "$entry" != ".." ]];then
+		if [ "$entry" == ".vim" ];
+		then
+			if [ -d "$HOME/$entry" ]
+			then
+				mv "$HOME/$entry" "$HOME/$entry.bak"
+			fi
+			ln -s $(realpath "$entry") "$HOME/$entry"
+		fi
+
+		if [ -d "$HOME/.config/$entry" ]
+		then
+			mv "$HOME/.config/$entry" "$HOME/.config/$entry.bak"
+		fi
+		ln -s $(realpath "$entry") "$HOME/.config/$entry"
+
+	fi
+done
