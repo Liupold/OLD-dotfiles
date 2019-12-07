edbc87c73f583ded2595661cc26b36bdeedf46bb
liupold <rohn.ch@gmail.com>
2019-12-07 08:43:28 +0530

    Made life a lit bit simpler.

--- home/scripts/system_managment/wallpaper_changer.sh
+++ home/scripts/system_managment/wallpaper_changer.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 REL_WALL_PATH=$1
-WALL_PATH=$(realpath $REL_WALL_PATH)
+set -e
+
+if [ -z "$REL_WALL_PATH" ]
+then
+
+	WALL_PATH="$(< "$HOME/.config/WALL_PATH")"
+
+else
+	WALL_PATH=$(realpath $REL_WALL_PATH)
+	echo "$WALL_PATH" > "$HOME/.config/WALL_PATH"
+fi
 
 wal -a 85 -i "$WALL_PATH"
-echo "$WALL_PATH" > "$HOME/.config/WALL_PATH"
-
-
-
