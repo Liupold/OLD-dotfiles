21be23a628fc9afae1a7f6f2b81754bdd9bfa401
liupold <rohn.ch@gmail.com>
2019-12-07 16:25:06 +0530

    making things better.

--- home/scripts/system_managment/usb_unmounter.sh
+++ home/scripts/system_managment/usb_unmounter.sh
@@ -1,13 +1,22 @@
 DRIVES="$(lsblk | grep -v 'sda.*$' | grep -v 'part $' | grep -o 's.*part' | awk '{ print $1 " ("$4")" }')"
+MTP_D="$(awk '/simple-mtpfs/ { print $2 }' /etc/mtab| sed -e 's|^|(MTP)|g')"
 
-SELECTED_DRIVE="$(echo "$DRIVES" | dmenu -p "Unmount?: " -fn "Fira Code-13" | awk '{ print $1 }')"
+SELECTED_DRIVE="$(echo "$DRIVES" "$MTP_D" | dmenu -p "Unmount?: " -fn "Fira Code-13" | awk '{ print $1 }')"
 
 if [ -z $SELECTED_DRIVE ]
 then
 	exit 0
 fi
 
-sudo umount "/dev/$SELECTED_DRIVE"
+if echo "$SELECTED_DRIVE" | grep -q "^(MTP).*"
+then
+	MOUNT_POINT=$(echo "$SELECTED_DRIVE" | sed -e "s|^(MTP)||g")
+	fusermount -u $MOUNT_POINT
+else
+	sudo umount "/dev/$SELECTED_DRIVE"
+fi
+
+
 if [ $? -eq 0 ]
 then
 	notify-send -a "USB-UNMOUNTER" "UnMounted" "$SELECTED_DRIVE is safe to remove."
