1c6166ee76ae52a27d480534ab773c5145cde50d
liupold <rohn.ch@gmail.com>
2020-05-18 18:27:43 +0530

    Now using dls

---
+++ packages.sh
@@ -0,0 +1,8 @@
+[ -f './dls' ] || \
+        curl "https://raw.githubusercontent.com/Liupold/dls/master/dls" > ./dls
+
+export dot_all='X11 alacritty bin bspwm dunst gtk-3.0 kitty
+luna_theme newsboat npm polybar ranger sxhkd sxiv vim zathura zsh'
+
+export dot_term='bin luna_theme newsboat npm ranger vim zsh'
+
