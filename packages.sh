354dbf0714944603cfcfd8a574adb634a9e966aa
liupold <rohn.ch@gmail.com>
2020-05-19 11:48:24 +0530

    made dls to be executable by default.

--- packages.sh
+++ packages.sh
@@ -1,5 +1,6 @@
 [ -f './dls' ] || \
         curl "https://raw.githubusercontent.com/Liupold/dls/master/dls" > ./dls
+chmod +x ./dls
 
 export dot_all='X11 alacritty bin bspwm dunst gtk-3.0 kitty
 luna_theme newsboat npm polybar ranger sxhkd sxiv vim zathura zsh'
