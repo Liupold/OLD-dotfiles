2fbb27b95f711d2eabecd94eab6c86b569a909c3
liupold <rohn.ch@gmail.com>
2020-05-23 12:46:55 +0530

    Improved vim config, lf

--- lf/.config/lf/pv.sh
+++ lf/.config/lf/pv.sh
@@ -4,12 +4,14 @@ case "$1" in
         *.zip) unzip -l "$1";;
         *.rar) unrar l "$1";;
         *.7z) 7z l "$1";;
-        *.pdf) pdftotext "$1" -;;
+        *.pdf) pdfinfo "$1";;
+        *.mp4|*.mkv|*.mp3|*.wav|*.m4a|*.vob)
+                ffprobe "$1" 2>&1 | grep -v -e '--\|ffprobe\|built\|\ *lib';;
         *)
                 echo FILE: "$1"
                 echo ------------------------------------------------------
                 cat -n "$1" | head -n "$(echo "$2 - 5" | bc)"
-                echo -e "\nTotal Lines: $(wc -l $1 | awk '{print $1}')"
-                echo "Permision: $(ls -la $1 | awk '{print $1}')";;
+                echo -e "\nTotal Lines: $(wc -l "$1" | awk '{print $1}')"
+                echo "Permision: $(ls -la "$1" | awk '{print $1}')";;
 esac
 
