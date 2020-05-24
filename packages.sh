8979fc4a2e4f89e54f168166e9303b30a0ff861d
liupold <rohn.ch@gmail.com>
2020-05-24 11:55:06 +0530

    making vim more comfy.

--- packages.sh
+++ packages.sh
@@ -3,7 +3,7 @@
 chmod +x ./dls
 
 export dot_all='X11 alacritty bin bspwm dunst gtk-3.0 kitty
-luna_theme newsboat npm polybar lf sxhkd sxiv vim zathura zsh cmus'
+luna_theme newsboat npm polybar lf sxhkd sxiv vim zathura zsh cmus mpv'
 
-export dot_term='bin luna_theme newsboat npm lf vim zsh cmus'
+export dot_term='bin luna_theme newsboat npm lf vim zsh cmus mpv'
 
