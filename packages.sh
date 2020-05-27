c9e3e92ed4133d0c55f6eccb4bd95d89d3af14ad
liupold <rohn.ch@gmail.com>
2020-05-27 12:49:37 +0530

    scripts are separated.

--- packages.sh
+++ packages.sh
@@ -2,8 +2,8 @@
         curl "https://raw.githubusercontent.com/Liupold/dls/master/dls" > ./dls
 chmod +x ./dls
 
-export dot_all='X11 alacritty bin bspwm dunst gtk-3.0 kitty
-luna_theme newsboat npm polybar lf sxhkd sxiv vim zathura zsh cmus mpv'
+export dot_all='X11 alacritty bin bspwm dunst gtk-3.0 kitty dmenu
+luna_theme newsboat npm polybar lf sxhkd sxiv vim nvim zathura zsh cmus mpv'
 
-export dot_term='bin luna_theme newsboat npm lf vim zsh cmus mpv'
+export dot_term='bin luna_theme newsboat npm lf vim nvim zsh cmus mpv'
 
