4f604e9b69d744023779208de5e9ecd8ff81ebbc
liupold <rohn.ch@gmail.com>
2020-05-28 09:03:26 +0530

    Completely switched to nvim, wirking on a install script

--- dots.sh
+++ dots.sh
@@ -4,9 +4,9 @@ chmod +x ./dls
 
 # dots in general
 export dot_all='X11 alacritty bin bspwm dunst gtk-3.0 kitty dmenu
-luna_theme newsboat npm polybar lf sxhkd sxiv vim nvim zathura zsh cmus mpv python'
+luna_theme newsboat npm polybar lf sxhkd sxiv nvim zathura zsh cmus mpv python'
 
-export dot_term='bin luna_theme newsboat npm lf vim nvim zsh cmus mpv python'
+export dot_term='bin luna_theme newsboat npm lf nvim zsh cmus mpv python'
 
 # in python pip
 export python_pip="$(grep --no-filename -e '^-P:' REQUIREMENT/archlinux/* | sed 's|^-P:||g' | uniq)"
@@ -14,4 +14,5 @@ export python_pip="$(grep --no-filename -e '^-P:' REQUIREMENT/archlinux/* | sed 
 # arch-linux (in official repo)
 export arch_linux_pacman="$(grep --no-filename -v '^-' REQUIREMENT/archlinux/* | uniq)"
 export arch_linux_aur="$(grep --no-filename -e '^-A:' REQUIREMENT/archlinux/* | sed 's|^-A:||g' | uniq) st-luke-git"
+
 
