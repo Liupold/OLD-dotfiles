f646c6e88885ada8d6022f925d66c2da3eb5c51d
liupold <rohn.ch@gmail.com>
2020-06-02 08:30:37 +0530

    improved requirement installer. WPC

--- luna_theme/.config/luna_theme/colors-tty.sh
+++ luna_theme/.config/luna_theme/colors-tty.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 [ "${TERM:-none}" = "linux" ] && \
-    printf '%b' '\e]P0131948
-                 \e]P193A6BC
-                 \e]P2D2B3BF
-                 \e]P3A5AFC4
-                 \e]P4D6AAC8
-                 \e]P5F0B6CE
-                 \e]P6F9CCD5
-                 \e]P7eee4e7
-                 \e]P8a69fa1
-                 \e]P993A6BC
-                 \e]PAD2B3BF
-                 \e]PBA5AFC4
-                 \e]PCD6AAC8
-                 \e]PDF0B6CE
-                 \e]PEF9CCD5
-                 \e]PFeee4e7
+    printf '%b' '\e]P020131f
+                 \e]P1B46250
+                 \e]P2956961
+                 \e]P3AC8679
+                 \e]P4D7AD72
+                 \e]P5B57684
+                 \e]P6C7AA9B
+                 \e]P7e1cbc2
+                 \e]P89d8e87
+                 \e]P9B46250
+                 \e]PA956961
+                 \e]PBAC8679
+                 \e]PCD7AD72
+                 \e]PDB57684
+                 \e]PEC7AA9B
+                 \e]PFe1cbc2
                  \ec'
