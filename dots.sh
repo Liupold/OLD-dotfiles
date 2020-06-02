235f05fc7a8bdcaa751a6cb7f8d62cb3eeba7746
liupold <rohn.ch@gmail.com>
2020-06-02 08:30:37 +0530

    improved requirement installer. WPC

--- dots.sh
+++ dots.sh
@@ -3,16 +3,8 @@
 chmod +x ./dls
 
 # dots in general
-export dot_all='X11 alacritty bin bspwm dunst gtk-3.0 kitty dmenu
+export dot_all='X11 alacritty bin bspwm dunst gtk-3.0 kitty dmenu ranger
 luna_theme newsboat npm polybar lf sxhkd sxiv nvim zathura zsh cmus mpv python'
 
 export dot_term='bin luna_theme newsboat npm lf nvim zsh cmus mpv python'
-
-# in python pip
-export python_pip="$(grep --no-filename -e '^-P:' REQUIREMENT/archlinux/* | sed 's|^-P:||g' | uniq)"
-
-# arch-linux (in official repo)
-export arch_linux_pacman="$(grep --no-filename -v '^-' REQUIREMENT/archlinux/* | uniq)"
-export arch_linux_aur="$(grep --no-filename -e '^-A:' REQUIREMENT/archlinux/* | sed 's|^-A:||g' | uniq) st-luke-git"
-
-
+export dot_gui_minimal='X11 bspwm dunst gtk-3.0 dmenu luna_theme sxhkd sxiv polybar mpv'
