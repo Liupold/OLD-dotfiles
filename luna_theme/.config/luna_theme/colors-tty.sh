4ded99ea07ea84ff4dfabd31c51f655a48146cc1
liupold <rohn.ch@gmail.com>
2020-06-07 19:57:02 +0530

    WPC beautiful rocket.

--- luna_theme/.config/luna_theme/colors-tty.sh
+++ luna_theme/.config/luna_theme/colors-tty.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 [ "${TERM:-none}" = "linux" ] && \
-    printf '%b' '\e]P0100d15
-                 \e]P1573D83
-                 \e]P2394586
-                 \e]P35A568B
-                 \e]P4996B98
-                 \e]P5C979A5
-                 \e]P6718B9A
-                 \e]P7e3c3cc
-                 \e]P89e888e
-                 \e]P9573D83
-                 \e]PA394586
-                 \e]PB5A568B
-                 \e]PC996B98
-                 \e]PDC979A5
-                 \e]PE718B9A
-                 \e]PFe3c3cc
+    printf '%b' '\e]P0080F37
+                 \e]P12F4775
+                 \e]P2A24952
+                 \e]P3E85D5A
+                 \e]P4295890
+                 \e]P55A6E9E
+                 \e]P6007CC3
+                 \e]P7afbbcc
+                 \e]P87a828e
+                 \e]P92F4775
+                 \e]PAA24952
+                 \e]PBE85D5A
+                 \e]PC295890
+                 \e]PD5A6E9E
+                 \e]PE007CC3
+                 \e]PFafbbcc
                  \ec'
