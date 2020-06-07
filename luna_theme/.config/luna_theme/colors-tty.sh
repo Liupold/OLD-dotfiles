da9f9283608ce3c22e5f27125ef1962649fab8d0
liupold <rohn.ch@gmail.com>
2020-06-07 19:32:52 +0530

    WPC

--- luna_theme/.config/luna_theme/colors-tty.sh
+++ luna_theme/.config/luna_theme/colors-tty.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 [ "${TERM:-none}" = "linux" ] && \
-    printf '%b' '\e]P020131f
-                 \e]P1B46250
-                 \e]P2956961
-                 \e]P3AC8679
-                 \e]P4D7AD72
-                 \e]P5B57684
-                 \e]P6C7AA9B
-                 \e]P7e1cbc2
-                 \e]P89d8e87
-                 \e]P9B46250
-                 \e]PA956961
-                 \e]PBAC8679
-                 \e]PCD7AD72
-                 \e]PDB57684
-                 \e]PEC7AA9B
-                 \e]PFe1cbc2
+    printf '%b' '\e]P0100d15
+                 \e]P1573D83
+                 \e]P2394586
+                 \e]P35A568B
+                 \e]P4996B98
+                 \e]P5C979A5
+                 \e]P6718B9A
+                 \e]P7e3c3cc
+                 \e]P89e888e
+                 \e]P9573D83
+                 \e]PA394586
+                 \e]PB5A568B
+                 \e]PC996B98
+                 \e]PDC979A5
+                 \e]PE718B9A
+                 \e]PFe3c3cc
                  \ec'
