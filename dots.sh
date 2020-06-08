72a9bd10dec5147bf8c719811387e512a34cdbff
liupold <rohn.ch@gmail.com>
2020-06-08 08:29:15 +0530

    fixed dots.sh

--- dots.sh
+++ dots.sh
@@ -3,8 +3,8 @@
 chmod +x ./dls
 
 # dots in general
-export dot_all='X11 alacritty bin bspwm dunst gtk-3.0 kitty dmenu ranger youtube-viewer
-luna_theme newsboat npm polybar lf sxhkd sxiv nvim zathura zsh cmus mpv python'
+export dot_all='X11 alacritty bin bspwm dunst gtk-3.0 kitty ranger youtube-viewer
+luna_theme newsboat npm polybar lf sxhkd sxiv nvim zathura zsh cmus mpv python emoji-menu'
 
 export dot_term='bin luna_theme newsboat npm lf nvim zsh cmus mpv python youtube-viewer'
-export dot_gui_minimal='X11 bspwm dunst gtk-3.0 dmenu luna_theme sxhkd sxiv polybar mpv'
+export dot_gui_minimal='X11 bspwm dunst gtk-3.0 luna_theme sxhkd sxiv polybar mpv emoji-menu'
