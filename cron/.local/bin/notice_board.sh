2670d3cf26297b71505c7fc8fcca225283ba2671
liupold <rohnch@protonmail.ch>
2020-10-03 12:51:02 +0530

    sepating cron.

---
+++ cron/.local/bin/notice_board.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+storage_file="$HOME/.cache/apc_notice"
+current_file="$(mktemp "/tmp/XXXXXX")"
+
+[ "$1" = '-d' ] && entry="$(sort -rg "$storage_file" | dmenu -b -l 10)" \
+        && /usr/bin/xdg-open "http://apccollege.ac.in/news/admin/uploads/${entry##* }"
+[ "$1" = '-d' ] && exit "$?"
+
+/usr/bin/curl --silent 'http://apccollege.ac.in/news/admin/uploads/?C=M;O=I' \
+        | while read -r entry; do
+                [ "$entry" = "${entry#*href*pdf}" ] && continue
+                entry="${entry#*href=\"}"
+                file_name="${entry%%\"*}"
+
+                entry="${entry#*align=\"right\">}"
+                file_date="${entry%%  </td>*}"
+
+                echo "$file_date" "$file_name"
+        done > "$current_file"
+
+[ ! -f "$storage_file" ] && /usr/bin/notify-send "APC notice" \
+        "empty cache, please check notice manually."
+
+[ -f "$storage_file" ] && /usr/bin/diff "$storage_file" "$current_file" |\
+        while read -r _change; do
+                [ "$_change" = "${_change#>}" ] && continue
+                _change="${_change#>}"
+                /usr/bin/notify-send  \
+                        "APC notice: $_change"\
+                        "http://apccollege.ac.in/news/admin/uploads/${_change##* }"
+        done
+mv "$current_file" "$storage_file"
--- dots.sh
+++ dots.sh
@@ -3,8 +3,8 @@ chmod +x ./dls
 
 
 # dots in general
-export dot_all='X11 alacritty bin bspwm dunst gtk-3.0 kitty ranger youtube-viewer
+export dot_all='X11 alacritty bin bspwm dunst gtk-3.0 kitty ranger youtube-viewer cron
 luna_theme newsboat npm polybar lf sxhkd sxiv nvim zathura zsh cmus mpv python emoji-menu'
 
-export dot_term='bin luna_theme newsboat npm lf nvim zsh cmus mpv python youtube-viewer'
+export dot_term='bin luna_theme newsboat npm lf nvim zsh cmus mpv python youtube-viewer cron'
 export dot_base="$dot_term X11 bspwm dunst gtk-3.0 luna_theme sxhkd sxiv polybar mpv emoji-menu"
