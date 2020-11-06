8d07aa9b79cfca2466649562abfae86a9f8fe108
liupold <rohnch@protonmail.ch>
2020-11-06 21:46:27 +0530

    cmus impv.

---
+++ cmus/.config/cmus/cmus-status.sh
@@ -0,0 +1,18 @@
+DATA="$@"
+DATAX="$@"
+
+DATA="${DATA##*status }"
+status="${DATA%% file*}"
+
+DATA="${DATA##*artist }"
+artist="${DATA%% title*}"
+artist="${artist%% album*}"
+
+DATA="${DATA##*title }"
+title="${DATA%% date*}"
+
+[ "$status" = "playing" ] && status=">"
+[ "$status" = "paused" ] && status="|"
+[ "$status" = "stopped" ] && status="."
+
+echo "CMUS:[ $status $title ($artist) ]" > /tmp/dwmbar
