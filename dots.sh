416eb189787b218c7d0f9dce0c612e3d558c7eac
liupold <rohnch@protonmail.ch>
2021-02-04 15:17:25 +0530

    minor test.

--- dots.sh
+++ dots.sh
@@ -1,7 +1,6 @@
 curl "https://raw.githubusercontent.com/Liupold/dls/master/dls" > ./dls
 chmod +x ./dls
 
-
 # dots in general
 export dot_all='X11 alacritty bin bspwm dunst gtk-3.0 kitty ranger youtube-viewer cron
 luna_theme newsboat npm polybar lf sxhkd sxiv nvim zathura zsh cmus mpv python emoji-menu'
