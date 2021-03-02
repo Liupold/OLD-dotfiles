b2c48f700fd6c50850073f1b1fb6c32b7e8377a5
liupold <rohnch@protonmail.ch>
2021-03-02 12:27:18 +0530

    minor changes.

--- python/.config/python/pystartup.py
+++ python/.config/python/pystartup.py
@@ -4,6 +4,7 @@ import os
 import readline
 
 histfile = os.path.join(os.path.expanduser("~/.cache/"), 'python_history')
+
 try:
     readline.read_history_file(histfile)
     readline.set_history_length(1000)
