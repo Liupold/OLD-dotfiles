7a1b5f5b34b25fa1afa6495b50086de26f08feff
Liupold <rohnch@protonmail.ch>
2021-05-21 07:48:01 +0530

    WPC

--- luna_theme/.config/luna_theme/colors-tty.sh
+++ luna_theme/.config/luna_theme/colors-tty.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 [ "${TERM:-none}" = "linux" ] && \
-    printf '%b' '\e]P00a1015
-                 \e]P1c46a31
-                 \e]P2bf692f
-                 \e]P332cc32
-                 \e]P4d5995a
-                 \e]P5256f96
-                 \e]P62c83b1
-                 \e]P7adbcc4
-                 \e]P8798389
-                 \e]P9c46a31
-                 \e]PAbf692f
-                 \e]PB32cc32
-                 \e]PCd5995a
-                 \e]PD256f96
-                 \e]PE2c83b1
-                 \e]PFadbcc4
+    printf '%b' '\e]P00c0b18
+                 \e]P12a85a9
+                 \e]P22c94b3
+                 \e]P33eb3ce
+                 \e]P46eda9e
+                 \e]P5e5b498
+                 \e]P6cbe597
+                 \e]P7eadbcb
+                 \e]P8a3998e
+                 \e]P92a85a9
+                 \e]PA2c94b3
+                 \e]PB3eb3ce
+                 \e]PC6eda9e
+                 \e]PDe5b498
+                 \e]PEcbe597
+                 \e]PFeadbcb
                  \ec'
