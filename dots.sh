4a75678d9376362047d946a66e198a77a4ed52eb
Liupold <rohnch@protonmail.ch>
2021-05-25 12:24:21 +0530

    fixed test

--- dots.sh
+++ dots.sh
@@ -1,5 +1,5 @@
 # Install
-./dls2 -S X11 alacritty bin bspwm cmus dunst emoji-menu gtk-3.0 kitty lf luna_theme mpv newsboat npm nvim octave polybar python ranger sxhkd sxiv youtube-viewer zathura zsh
+./dls2 -S X11 alacritty bin bspwm cmus dunst emoji-menu gtk-3.0 kitty lf luna_theme mpv newsboat npm nvim octave polybar python ranger sxhkd sxiv youtube-viewer zathura zsh pulse
 
 # Uninstall
 # ./dls2 -D X11 alacritty bin bspwm cmus dunst emoji-menu gtk-3.0 kitty lf luna_theme mpv newsboat npm nvim octave polybar python ranger sxhkd sxiv youtube-viewer zathura zsh
