7b4a976d60004aa26402efe8c26cfc8369c56d7a
Liupold <rohnch@protonmail.ch>
2021-06-25 15:37:47 +0530

    WPC cmus status update

--- cmus/.config/cmus/cmus-status.sh
+++ cmus/.config/cmus/cmus-status.sh
@@ -16,7 +16,6 @@ title="${DATA%% date*}"
 [ "$status" = "paused" ] && status="|"
 [ "$status" = "stopped" ] && status="."
 
-[ "$DATA" = "$artist" ] && artist=""
-[ "$DATA" = "$title" ] && title="file: \"${DATA#*file }\""
+[ "$title" = "$artist" ] && artist=""  && title="file: \"${DATA#*file }\""
 
 echo "CMUS:[ $status $title ($artist) ]" > /tmp/dwmbar
